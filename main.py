53edc59db3fb306733aa68f0e8a793af57280fbe
C04H03 <C04H03@teb.edu.pl>
2024-04-09 11:12:08 +0200

    taaa

--- main.py
+++ main.py
@@ -1,57 +1,97 @@
+import random
 import string
 import sys
-import random
 
-from PyQt6.QtWidgets import QApplication, QDialog
+from PyQt6.QtWidgets import QDialog, QApplication
 
 from layout import Ui_Dialog
 
 
 class MyForm(QDialog):
+
 	def __init__(self):
 		super().__init__()
 		self.ui = Ui_Dialog()
 		self.ui.setupUi(self)
 		self.ui.generate.clicked.connect(self.generate)
+		self.ui.easter.clicked.connect(self.easterClicked)
 
-		self.smallChar = [l for l in string.ascii_lowercase]
-		self.capitalchar = [l for l in string.ascii_uppercase]
-		self.numbers = [l for l in string.digits]
+		self.smallChars = [l for l in string.ascii_lowercase]
+		self.capitalChars = [l for l in string.ascii_uppercase]
+		self.numbers = [str(i) for i in range(0, 10)]
 		self.specialChars = [l for l in string.punctuation]
 
 	def generate(self):
-		lenght = self.ui.passowrdLenght.text()
-		type = self.ui.passowrdType.currentText()
-		password = ''
-		if type == "Pin":
+		while True:
+			length = int(self.ui.passwordLength.text())
+			password_type = self.ui.passwordType.currentText()
+			password = ''
+			counter = 0
 
-			for i in range(int(lenght)):
-				password += str(random.randint(0, 9))
-		else:
-			elements = [self.smallChar]
-			if self.ui.specialChar.isChecked():
-				elements.append(self.specialChars)
-			if self.ui.number.isChecked():
-				elements.append(self.numbers)
-			if self.ui.capitalChards.isChecked():
-				elements.append(self.capitalchar)
-			for i in range(int(lenght)):
-				type = random.randint(0,len(elements)-1)
-				password += elements[type][random.randint(0, len(elements[type])-1)]
+			if password_type == 'Pin':
+				for i in range(length):
+					password += str(random.randint(0, 9))
+			else:
+				elements = []
+				elements.append(self.smallChars)
+				if self.ui.capitalChars.isChecked():
+					counter += 1
+					elements.append(self.capitalChars)
+				if self.ui.numbers.isChecked():
+					counter += 1
+					elements.append(self.numbers)
+				if self.ui.specialChar.isChecked():
+					counter += 1
+					elements.append(self.specialChars)
+
+				if int(length) < counter:
+					print("Za mało")
+					return
+
+				for i in range(length):
+					element_type = random.choice(elements)
+					password += random.choice(element_type)
 
 			if self.ui.easter.isChecked() and len(password) >= 5:
 				max_index = len(password) - 6
 				start_index = random.randint(0, max_index)
-				easter = 'zając'
 				new_password = ''
+				easter = 'zając'
 				for i in range(len(password)):
-					if start_index <= i < start_index+5:
+					if start_index <= i < start_index + 5:
 						new_password += easter[i - start_index]
 					else:
 						new_password += password[i]
 				password = new_password
 
-		self.ui.generatedpassword.setText(password)
+			if self.check_password(password):
+				self.ui.genertedPassword.setText(password)
+				break
+
+	def check_password(self, password):
+		if self.ui.capitalChars.isChecked() and not any(char.isupper() for char in password):
+			return False
+		if self.ui.numbers.isChecked() and not any(char.isdigit() for char in password):
+			return False
+		if self.ui.specialChar.isChecked() and not any(char in string.punctuation for char in password):
+			return False
+		return True
+
+	def easterClicked(self):
+		if self.ui.easter.isChecked():
+			self.ui.numbers.setChecked(False)
+			self.ui.capitalChars.setChecked(False)
+			self.ui.specialChar.setChecked(False)
+			self.ui.word.setChecked(False)
+			self.ui.numbers.setDisabled(True)
+			self.ui.capitalChars.setDisabled(True)
+			self.ui.specialChar.setDisabled(True)
+			self.ui.word.setDisabled(True)
+		else:
+			self.ui.numbers.setDisabled(False)
+			self.ui.capitalChars.setDisabled(False)
+			self.ui.specialChar.setDisabled(False)
+			self.ui.word.setDisabled(False)
 
 
 if __name__ == "__main__":
